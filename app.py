071c8d6f4116f56499449b58d9f0542153a5f045
IDGS-802-23002541 <83322@alumnos.utleon.edu.mx>
2026-02-18 20:53:55 -0600

    coneccion a la base de datos

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 from flask import Flask, render_template
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
+from config import DevelopmentConfig
+from flask import g
+import forms
+
+from models import db
+from models import Alumnos
 
 app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+db.init_app(app)
+csrf=CSRFProtect()
 
 @app.route("/")
 @app.route("/index")
 def index():
 	return render_template("index.html")
 
+@app.route('/alumnos')
+def alumnos():
+	return render_template("Alumnos.html")
+
+	
+@app.errorhandler(404)
+def page_not_fount(e):
+	return render_template("404.html"), 404
+
 if __name__ == '__main__':
+	csrf.init_app(app)
+	with app.app_context():
+		db.create_all()
 	app.run(debug=True)
+
